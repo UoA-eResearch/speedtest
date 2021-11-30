7552143bf3ac3759aff12bcdb555be6d5879a155
Nick Young <nick.young@auckland.ac.nz>
2021-11-30 13:31:14 +1300

    add a test for OneDrive

--- run.py
+++ run.py
@@ -75,7 +75,46 @@ def test_gdrive():
     print(f"Download: {round(100 / (time.time() - s), 2)}MB/s")
     file.Delete()
 
+def test_onedrive():
+    print("Testing OneDrive")
+    import onedrivesdk
+    redirect_uri = 'http://localhost:8000/callback'
+    client_secret = '86l7Q~zwmQ2Qc5zWHf9pWWUGBW2-6RxYP.r.p'
+    client_id='0cda2cfb-622f-49c4-b48c-9a772db82f27'
+    api_base_url='https://api.onedrive.com/v1.0/'
+    scopes=['onedrive.readwrite']
+
+    http_provider = onedrivesdk.HttpProvider()
+    auth_provider = onedrivesdk.AuthProvider(
+        http_provider=http_provider,
+        client_id=client_id,
+        scopes=scopes)
+
+    client = onedrivesdk.OneDriveClient(api_base_url, auth_provider, http_provider)
+    try:
+        auth_provider.load_session()
+    except FileNotFoundError:
+        auth_url = auth_provider.get_auth_url(redirect_uri)
+        # Ask for the code
+        print('Paste this URL into your browser, approve the app\'s access.')
+        print('Copy everything in the address bar after "code=", and paste it below.')
+        print(auth_url)
+        code = input('Paste code here: ')
+        auth_provider.authenticate(code, redirect_uri, client_secret)
+        auth_provider.save_session()
+    with open("100MB", "wb") as f:
+        f.write(testfile)
+    s = time.time()
+    file = client.item(drive='me', id='root').children['100MB'].upload('100MB')
+    print(f"Upload: {round(100 / (time.time() - s), 2)}MB/s")
+    s = time.time()
+    client.item(drive='me', id=file.id).download('100MB')
+    print(f"Download: {round(100 / (time.time() - s), 2)}MB/s")
+    client.item(id=file.id).delete()
+    os.unlink("100MB")
+
 test_speedtest()
 test_unifiles()
 test_dropbox()
 test_gdrive()
+test_onedrive()
